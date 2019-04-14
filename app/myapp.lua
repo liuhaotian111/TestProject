3ff86d42df5311902bfa6b118c9f97917b14e8f1
DESKTOP-O68JEAL\haotianliu <568144935@qq.com>
2019-04-14 17:36:38 +0800

    运算符

--- app/myapp.lua
+++ app/myapp.lua
@@ -1,17 +1,2 @@
-local hahah = {}
-for i=1,10 do
-	table.insert(hahah,i)
-end
-
-for i,v in ipairs(hahah) do
-	print(i,v)
-end
-
-
-table.sort( hahah, function (a, b)
-	return a>b
-end )
-
-for i,v in ipairs(hahah) do
-	print('===>>',i,v)
-end
+local x = 100
+print(x > 100)
